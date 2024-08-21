5f64fa42899d3a2d66769d447981dd6e5398fee5
Marc985 <hei.marc.3@gmail.com>
2024-08-21 11:14:09 +0300

    chore:add framer motion

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,13 +1,21 @@
 import Project1 from "../assets/imgs/project.png"
-import WebCupImg from "../assets/imgs/webcup.png"
 import { useTranslation } from "react-i18next"
 import BankIMg from "../assets/imgs/bankImg.png"
 import RestaurantImg from "../assets/imgs/restaurantImg.png"
 import madaGreen from "../assets/imgs/mada-green.png"
+import { useState } from "react"
+import { FaArrowDownLong } from "react-icons/fa6";
+import SuperPower from "../assets/imgs/webcup.png"
+import { motion } from "framer-motion"
+import { useInView } from "framer-motion"
+import { useRef } from "react"
 
 const Project =()=>{
 
     const {t}=useTranslation()
+    const [showAll,setShowAll]=useState(false)
+    const sectionRef = useRef(null); // Créer une référence pour la section projet
+    const isInView = useInView(sectionRef, { once: true });
 
 const projectList=[
     {
@@ -23,13 +31,7 @@ const projectList=[
         img:madaGreen,
         link:"https://environment-inky.vercel.app/"
     },
-   
-    {
-        title:"wallet",
-        description:t('project3Desc'),
-        img:BankIMg,
-        link:null
-    },
+ 
  
     {
         title:"restaurant website",
@@ -37,26 +39,67 @@ const projectList=[
         img:RestaurantImg,
         lnk:"https://marc985.github.io/restaurant_website/"
     },
+      
+    {
+        title:"wallet",
+        description:t('project3Desc'),
+        img:BankIMg,
+        link:null
+    },
+    {
+        title:"super power swap",
+        description:t('project5Desc'),
+        img:SuperPower,
+        link:null
+    }
 
 ]
+    const projectToShow=showAll?projectList:projectList.slice(0,3)
     return (
     <div className="dark:text-gray-200 cursor-pointer">
         <h1 className="text-center text-4xl my-4">{t("project")}</h1>
-        <div className="grid grid-cols-1 gap-6 w-[80%] mx-auto my-10 md:grid-cols-3">
-           {// eslint-disable-next-line array-callback-return
-            
-            projectList.map((project)=>(
-                 <ProjectCard title={project.title} description={project.description} img={project.img} link={project.link}/>
-            ))
-           }
+        <div className="grid grid-cols-1 gap-6 w-[80%] mx-auto my-10 md:grid-cols-3" ref={sectionRef}>
+         {projectToShow.map((project, index) => (
+          <motion.div
+            key={project.title}
+            initial={{ scale: 0 }}
+            animate={isInView ? { scale: 1 } : { rotate:360,scale: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+              delay: index * 0.5, // Ajoute un délai pour chaque projet
+            }}
+          >
+            <ProjectCard
+              
+              title={project.title}
+              description={project.description}
+              img={project.img}
+              link={project.link}
+            />
+          </motion.div>
+        ))}
         </div>
+        {!showAll && (
+        <div className="text-center">
+          <button
+            onClick={() => setShowAll(true)} // Lorsqu'on clique, on montre tous les projets
+            className="py-1 px-4 text-gray-600 dark:text-white border border-gray-600 dark:border-white rounded-lg dark:hover:bg-slate-300-700 transition flex flex-row gap-1 items-center justify-center mx-auto"
+          >
+            <p>{t("see")}</p>
+            <FaArrowDownLong/>
+          </button>
+        </div>
+      )}
+    
     </div>
     )
 }
 const ProjectCard = ({ title, description, img, link }) => {
     return (
         <a
-            className="flex flex-col gap-2 shadow-lg border h-96 rounded-lg dark:border-none dark:bg-gray-900"
+            className="flex flex-col gap-2 shadow-lg border h-96 rounded-lg dark:border-none dark:bg-gray-900 transform transition-all duration-500"
             target={link ? "_blank" : undefined}
             rel={link ? "noreferrer" : undefined}
             href={link}
