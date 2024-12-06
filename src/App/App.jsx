7bf54d6f26d593e639232aaaf8dbb1125213d900
Marc985 <hei.marc.3@gmail.com>
2024-12-06 15:54:48 +0300

    optimize SEO

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,22 @@
 import "./App.scss";
 import React, { useEffect, useState } from "react";
-import NavBar from "../Components/NavBar";
-import Home from "../Components/Home";
-import About from "../Components/About";
-import Project from "../Components/Project";
-import Stack from "../Components/Stack";
-import Footer from "../Components/Footer";
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdOutlineDarkMode } from "react-icons/md";
 import ReactGA from 'react-ga4';
 import { Helmet } from "react-helmet";
+import{ lazy, Suspense } from "react";
+
 
 const App = () => {
+    const NavBar=lazy(()=>import("../Components/NavBar"));
+    const Home=lazy(()=>import("../Components/Home"));
+    const About=lazy(()=>import("../Components/About"));
+    const Project=lazy(()=>import("../Components/Project"));
+    const Stack=lazy(()=>import("../Components/Stack"));
+    const Footer=lazy(()=>import("../Components/Footer")); 
+
+
+
     const [isDarkMode, setIsDarkMode] = useState(() => {
         const savedMode = localStorage.getItem('dark-mode');
         return savedMode ? JSON.parse(savedMode) : false;
@@ -36,12 +41,32 @@ const App = () => {
     };
 
     return (
-      <div>
+      <Suspense fallback={"...loading"}>
                 <div className={`transition-all duration-700 ${isDarkMode ? 'dark:bg-gray-900' : 'bg-white'} w-full`}>
                     <Helmet>
-                    <meta charSet="utf-8" />
-                        <title>Developpeur web</title>
+                    <meta charSet="utf-8" 
+                        name="description"
+                        content="Je suis Marc Herilala, un developpeur web , j'aime créer des sites web modernes et performants. Je suis à la recherche de nouvelles opportunités de travail."
+                    />
+                        <title>Marc Herilala | Developpeur web</title>
                         <link rel="canonical" href="https://herilala.vecel.app" />
+                        <script type="application/ld+json">
+                            {JSON.stringify({
+                            "@context": "http://schema.org",
+                            "@type": "Person",
+                            "name": "Marc Herilala Razanajatovo",
+                            "url": "https://herilala.vercel.app",
+                            "sameAs": [
+                                "https://linkedin.com/in/marc-razanajatovo",
+                                "https://github.com/Marc985"
+                            ],
+                            "jobTitle": "Développeur Web",
+                            "worksFor": {
+                                "@type": "Organization",
+                                "name": "Haute Ecole d'Informatique Madagascar"
+                            }
+                            })}
+                        </script>
                     </Helmet>
 
                     <NavBar onClick={toggleDarkMode} icon={icon} />
@@ -51,7 +76,7 @@ const App = () => {
                     <Stack />
                     <Footer />
                 </div>
-      </div> 
+      </Suspense> 
     );
 };
 
