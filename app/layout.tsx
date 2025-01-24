786a6ca73ea5cfc5de1e98629cf19b45a543179f
Marc985 <hei.marc.3@gmail.com>
2025-01-24 14:39:41 +0300

    add portfolio screen on the opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://herilala.vercel.app/portfolio.png",  // Remplace par le lien de ton image
+        url: "https://herilala.vercel.app/portfolio.png", 
         width: 1200,
         height: 630,
         alt: "Photo de Marc Herilala",
